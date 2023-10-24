d2bfc56097a020c36b37f34d937b6f0b3163b1c7
Rahul_islam <103566678+Rahulislam1177@users.noreply.github.com>
2023-10-24 20:54:23 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './components/Home/Home'
+import Nabvar from './components/Shared/Nabvar/Nabvar'
+
+function App() {
+return (
+    <>
+      <Nabvar></Nabvar>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
